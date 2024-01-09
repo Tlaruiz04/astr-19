767be50a1412f56b78dda86ca260711614d5863c
Tlaruiz04 <146769405+Tlaruiz04@users.noreply.github.com>
2024-01-09 14:11:04 -0800

    Add files via upload

---
+++ myname.py
@@ -0,0 +1,10 @@
+def WhoIAm():
+    print("Name: Tlaloc Ruiz")
+    print('Pronouns: He/Him/His')
+    print('Favorite Movie: I can not say I have a favorite though I enjoy The Cat in the Hat')
+    print('Favorite Food: I would have to go with the Burrito')
+def main():
+    WhoIAm()
+    
+if __name__=="__main__":
+    main()
