22a474bb95fc31d13d19406a8822e049aa9eefd5
Tlaruiz04 <146769405+Tlaruiz04@users.noreply.github.com>
2024-01-09 14:30:28 -0800

    Add files via upload

---
+++ mathstuff.py
@@ -0,0 +1,14 @@
+def Mathstuff():
+    x = 0.625 + 3.756 
+    print(x)
+    print(type(x))
+    y = 5 - 3
+    print(y)
+    print(type(y))
+    z = 0.876 * 5
+    print(z)
+    print(type(z))
+def Main():
+    Mathstuff()
+if __name__ == "__main__":
+    Main()
