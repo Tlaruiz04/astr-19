3a1955295c34348d62584ee3704e6de60c010882
Tlaruiz04 <146769405+Tlaruiz04@users.noreply.github.com>
2024-01-23 18:21:56 -0800

    Add files via upload

---
+++ prompt_3.py
@@ -0,0 +1,11 @@
+def f(x):
+    y = (x**3) + 8  
+    return y
+def main():
+    result = f(9)
+    print(result)
+    if result > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()
